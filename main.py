27efd55b61bb123a42679a78a872debde9bdeef6
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2021-08-01 20:11:26 -0700

    bug fix

--- main.py
+++ main.py
@@ -30,19 +30,6 @@ def character_2k(text):  # See If CopyPasta Have Over 2,000 Characters
         print("This Copypasta Has Over 2,000 Characters, {} Characters in Total".format(len(text)))
 
 
-def copypasta(text):
-    choice_copy = input(
-        "\nDo You Want The Text To Be Copied? [y/n]: ").lower()
-
-    if choice_copy == "y" or choice_copy == "yes":
-        pyperclip.copy(text)
-    elif choice_copy == "n" or choice_copy == "no":
-        return
-    else:
-        print("Enter 'Yes' or 'No'")
-        return
-
-
 def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypastatext.com/"
     page_number = 1  # Set Default Page Number To 1
     while True:
@@ -54,18 +41,18 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
         site_content = soup.find_all("div", class_="excerpt-wrap entry-summary")  # Find CopyPasta Content
         site_page_number = soup.find("ul", class_="page-numbers")  # Find Page Number
 
-        clear_screen()
         if len(site_title) <= 0 or len(site_content) <= 0:  # If Cannot Find Result, Search With New Input
             print("Could Not Find Copypasta With That Search")
             search_input = input("What Copypasta Do You Want To Search: ")
+            page_number = 1
         else:  # If Search Input Found, Continue
             try:
                 site_max_page = max(list(map(int, re.findall(r'\d+', site_page_number.text))))
                 site_min_page = min(list(map(int, re.findall(r'\d+', site_page_number.text))))
-                print("Page {} / {}".format(page_number, site_max_page))
+                print("\nPage {} / {}".format(page_number, site_max_page))
             # AttributeError: 'NoneType' object has no attribute 'text' ; If Only 1 Page Exist, print("Page 1 / 1")
             except AttributeError:
-                print("Page 1 / 1")
+                print("\nPage 1 / 1")
                 site_max_page = 1
                 site_min_page = 1
 
@@ -76,7 +63,7 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
 
             for index in range(1, len(site_title)+1):  # Prints The Title With Number ex. "1 Example CopyPasta"
                 print(index, site_title[index-1].text)
-            print("\n'N' Goto The Next Page For Copypasta\n'P' Goto The Previous Page For Copypasta")
+            print("\n'N' Goto The Next Page For Copypasta\n'P' Goto The Previous Page For Copypasta\n'E' Exit The Program\n'R' Restart The Search")
 
             choice_select = input("Which One Do You Want: ")
             try:
@@ -89,14 +76,33 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
                     return
                 else:  # Check if "choice_select" = to 0, If It Does = to 0 Do Not - 1, Else - 1
                     choice_select -= 1
-                    print(type(choice_select), choice_select)
-
                     copypasta_text = site_content[choice_select].text
 
                     clear_screen()
                     print(copypasta_text)
                     character_2k(copypasta_text)
-                    copypasta(copypasta_text)
+
+                choice_copy = input(
+                    "\nDo You Want The Text To Be Copied? [yes/no/restart]: ").lower()
+
+                if choice_copy == "yes" or choice_copy == "y":
+                    pyperclip.copy(copypasta_text)  # Using pyperclip Module Because It Copies Emojis Correctly
+                    choice_search_again = input("Go Search Again? [yes/no]").lower()
+                    if choice_search_again == "yes" or choice_search_again == "y":
+                        clear_screen()
+                        search_input = input("What Copypasta Do You Want To Search: ")
+                        page_number = 1
+                    elif choice_search_again == "no" or choice_search_again == "n":
+                        return
+
+                elif choice_copy == "no" or choice_copy == "n":
+                    return
+                elif choice_copy == "restart" or choice_copy == "r":
+                    clear_screen()
+                    search_input = input("What Copypasta Do You Want To Search: ")
+                    page_number = 1
+                else:
+                    print("Enter 'Yes', 'No' or 'Restart'")
                     return
 
             except ValueError:
@@ -105,7 +111,6 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
                     if page_number >= site_max_page:
                         clear_screen()
                         print("Cannot Go To The Next Page When You Are On The Last Page")
-                        os.system("pause")
                     else:
                         page_number += 1
                         clear_screen()
@@ -114,10 +119,20 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
                     if page_number <= site_min_page:
                         clear_screen()
                         print("Cannot Go To The Previous Page When You Are On The First Page")
-                        os.system("pause")
                     else:
                         page_number -= 1
                         clear_screen()
+
+                elif choice_select == "r" or choice_select == "restart":
+                    clear_screen()
+                    search_input = input("What Copypasta Do You Want To Search: ")
+                    page_number = 1
+
+                elif choice_select == "e" or choice_select == "exit":
+                    clear_screen()
+                    print("Exiting")
+                    os.system("pause")
+                    return
 
                 else:
                     print("Please Enter Only Enter Options From The Prompt")
@@ -129,4 +144,5 @@ def CopyPastaText_Com(search_input):  # Search CopyPasta On Site "https://copypa
                 return
 
 
+clear_screen()
 CopyPastaText_Com(input("What Copypasta Do You Want To Search: "))
